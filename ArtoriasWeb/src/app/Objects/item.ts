1ff7863f68be7e803da721850ef12ae2abca5ca7
Carl-Nelson <50152324+Carl-Nelson@users.noreply.github.com>
2021-10-28 18:36:03 -0700

    Update item.ts
    Added the type property, and some comments

--- ArtoriasWeb/src/app/Objects/item.ts
+++ ArtoriasWeb/src/app/Objects/item.ts
@@ -1,14 +1,16 @@
 
 export class Item{
     name: string;
-    status: string;
-    priority: number;
-    description: string;
+    status: string; //new, open, closed, etc. Whatever column it goes in.
+    priority: number; // user-defined priority
+    description: string; //optional description
+    type: string; //Epic, feature, user story, issue, whatever.
 
-    constructor(name: string, status: string, priority: number, description?: string){
+    constructor(name: string, status: string, priority: number, type: string, description?: string){
         this.name = name;
         this.status = status;
         this.priority = priority;
+        this.type = type;
         if (description) {
             this.description = description;
         } 
