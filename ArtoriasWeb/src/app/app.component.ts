855e22026ab99e99cdf9fdeea76cc70b4f41fc3d
Darren Ross <darren.ross@bellevuecollege.edu>
2021-11-30 23:16:32 -0800

    Minor tweaks.

--- ArtoriasWeb/src/app/app.component.ts
+++ ArtoriasWeb/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DataService } from './data.service';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { NewProjectComponent } from './new-project/new-project.component';
 import { NewUserComponent } from './new-user/new-user.component';
 
 @Component({
@@ -26,6 +27,18 @@ export class AppComponent {
     const dialogRef = this.dialog.open(NewUserComponent, {
       width: '500px',
       data: {}, //nothing here yet
+    });
+  }
+
+  createNewProject() {
+    const dialogRef = this.dialog.open(NewProjectComponent, {
+      width: '500px',
+      data: {}, //nothing here yet
+    });
+    
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      console.log(result);
     });
   }
 
